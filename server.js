e5583ab1fad67204ae843470319a55633fe34c00
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2023-12-19 21:15:23 -0500

    next commit

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Log = require('./models/logs');
-const methodOverride = require('method-override');
-const logsController = require('./controllers/logsController');
-const logsEditController = require('./controllers/logsEditController');
-
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Log = require("./models/logs");
+const methodOverride = require("method-override");
+const logsController = require("./controllers/logsController");
+const logsEditController = require("./controllers/logsEditController");
 
 const app = express();
 const PORT = process.env.PORT || 5005;
@@ -20,23 +19,19 @@ mongoose.connect(mongoURI, {
 
 const db = mongoose.connection;
 
-db.on('error', err => console.log(err.message + " is mongod not running?"));
-db.on('open', () => console.log('mongo connected!'));
-db.on('close', () => console.log('mongo disconnected'));
+db.on("error", (err) => console.log(err.message + " is mongod not running?"));
+db.on("open", () => console.log("mongo connected!"));
+db.on("close", () => console.log("mongo disconnected"));
 
-app.use(methodOverride('_method'));
-app.use(express.static('public'));
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 
-app.use('/logs', logsController);
-app.use('/logs', logsEditController);
-
-
-
+app.use("/logs", logsController);
+app.use("/logs", logsEditController);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
